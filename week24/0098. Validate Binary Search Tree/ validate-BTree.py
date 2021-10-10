c95e6cf66750cd5369ba2f2c1767edee78f34577
kenwoodjw <blackxin55+@gmail.com>
2021-10-10 15:02:37 +0800

    week24

---
+++ week24/0098. Validate Binary Search Tree/ validate-BTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# 中序遍历
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack, inorder = [], float('-inf')
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+
+        return True
+
---
+++ week24/README.md
@@ -0,0 +1,3 @@
+[98. 验证二叉搜索树]("https://leetcode-cn.com/problems/validate-binary-search-tree/")
+
+[235. 二叉搜索树的最近公共祖先]("https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/")
